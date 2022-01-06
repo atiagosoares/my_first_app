443cfd9023a054937097a7bbda5950fc35c95952
unknown <atiagosoares@gmail.com>
2022-01-05 22:43:23 -0300

    Fixed game-info bug; improved visual on mobile

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from whoami import socket, app
         
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    socket.run(app, debug=False)
+    socket.run(app, debug=True)
